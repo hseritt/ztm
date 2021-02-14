5668a93f087f2e30063db682a3c62fb93dd99478
Harlin Seritt <harlin.seritt@gmail.com>
2021-02-13 20:03:27 -0700

    Misc adds 15Feb21 part 2

---
+++ JavaScript/Facebook1/index.js
@@ -0,0 +1,24 @@
+
+var user = {
+	username: "hseritt",
+	password: "s3cr3t"
+};
+
+var database = [
+	user
+];
+
+var feed = [
+	{
+		username: "hseritt",
+		timeline: "What is up?!"
+	},
+	{
+		username: "hseritt",
+		timeline: "Look at this picture of my food!"
+	},
+	{
+		username: "hseritt",
+		timeline: "Oops I forgot how to upload a photo! LOL!"
+	}
+];
